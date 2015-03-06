b42f7a1a7eebd5662b5c6d30af4fbf20a59a44a4
Graham Watson <graham@phantomwatson.com>
2015-03-05 23:18:52 -0500

    Convert spaces to tabs

--- src/Model/Entity/Thought.php
+++ src/Model/Entity/Thought.php
@@ -9,32 +9,32 @@ use Cake\ORM\Entity;
 class Thought extends Entity
 {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        'user_id' => true,
-        'word' => true,
-        'thought' => true,
-        'color' => true,
-        'time' => true,
-        'edited' => true,
-        'comments_enabled' => true,
-        'parsedTextCache' => true,
-        'cacheTimestamp' => true,
-        'anonymous' => true,
-        'user' => true,
-        'comments' => true,
-    ];
-    public $max_thoughtword_length = 30;
+	/**
+	 * Fields that can be mass assigned using newEntity() or patchEntity().
+	 *
+	 * @var array
+	 */
+	protected $_accessible = [
+		'user_id' => true,
+		'word' => true,
+		'thought' => true,
+		'color' => true,
+		'time' => true,
+		'edited' => true,
+		'comments_enabled' => true,
+		'parsedTextCache' => true,
+		'cacheTimestamp' => true,
+		'anonymous' => true,
+		'user' => true,
+		'comments' => true,
+	];
+	public $max_thoughtword_length = 30;
 
-    /**
-     * Converts $word into a valid thoughtword (alphanumeric, no spaces, max 30 characters)
-     * @param string $word
-     * @return string
-     */
+	/**
+	 * Converts $word into a valid thoughtword (alphanumeric, no spaces, max 30 characters)
+	 * @param string $word
+	 * @return string
+	 */
 	public function format_thoughtword($word) {
 		$word = preg_replace('/[^a-zA-Z0-9]/', '', $word);
 		if (strlen($word) > $this->max_thoughtword_length) {
