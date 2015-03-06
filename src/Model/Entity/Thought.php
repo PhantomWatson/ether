1c6633f3cf119cc5af1d9b5092b862779700b8f0
Graham Watson <graham@phantomwatson.com>
2015-03-05 23:18:30 -0500

    Add format_thoughtword()

--- src/Model/Entity/Thought.php
+++ src/Model/Entity/Thought.php
@@ -28,4 +28,18 @@ class Thought extends Entity
         'user' => true,
         'comments' => true,
     ];
+    public $max_thoughtword_length = 30;
+
+    /**
+     * Converts $word into a valid thoughtword (alphanumeric, no spaces, max 30 characters)
+     * @param string $word
+     * @return string
+     */
+	public function format_thoughtword($word) {
+		$word = preg_replace('/[^a-zA-Z0-9]/', '', $word);
+		if (strlen($word) > $this->max_thoughtword_length) {
+			$word = substr($word, 0, $this->max_thoughtword_length);
+		}
+		return strtolower($word);
+	}
 }
