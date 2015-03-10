1bbea144177363722a5be92001cd8469a4a5778a
Graham Watson <graham@phantomwatson.com>
2015-03-10 15:50:08 -0400

    Move methods from entity to table
    Because I'm not sure how the hell to call arbitrary entity methods and
    table methods are available from the controller as
    $this->Thoughts->methodName().

--- src/Model/Table/ThoughtsTable.php
+++ src/Model/Table/ThoughtsTable.php
@@ -236,4 +236,34 @@ class ThoughtsTable extends Table
 		$combined_query = $thoughts_query->unionAll($comments_query);
 		return $combined_query;
 	}
+
+	/**
+	 * Converts $word into a valid thoughtword (alphanumeric, no spaces, max 30 characters)
+	 * @param string $word
+	 * @return string
+	 */
+	public function format_thoughtword($word) {
+		$word = preg_replace('/[^a-zA-Z0-9]/', '', $word);
+		if (strlen($word) > $this->max_thoughtword_length) {
+			$word = substr($word, 0, $this->max_thoughtword_length);
+		}
+		return strtolower($word);
+	}
+
+	/**
+	 * Checks to see if the thought in $this->request->data is already in the database
+	 * @return int|boolean Either the ID of the existing thought or FALSE
+	 */
+	public function isDuplicate() {
+		$user_id = $this->request->data['Thought']['user_id'];
+		$thought = $this->request->data['Thought']['thought'];
+		$thoughts = TableRegistry::get('Thoughts');
+		$query = $thoughts->findByUserIdAndThought($user_id, $thought);
+		$query
+			->select(['id'])
+			->order(['Thought.created' => 'DESC'])
+			->first();
+		$results = $query->toArray();
+		return isset($results['Thought']['id']) ? $results['Thought']['id'] : false;
+	}
 }
