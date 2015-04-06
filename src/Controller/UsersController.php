c03e86d43f9c1b9ab857a5ba9663f5818b106b79
Graham Watson <graham@phantomwatson.com>
2015-04-06 19:25:02 -0400

    Add login()

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -98,4 +98,19 @@ class UsersController extends AppController
         }
         return $this->redirect(['action' => 'index']);
     }
+
+	public function login() {
+		if ($this->request->is('post')) {
+			$user = $this->Auth->identify();
+			if ($user) {
+				$this->Auth->setUser($user);
+				return $this->redirect($this->Auth->redirectUrl());
+			} else {
+				$this->Flash->error('Email or password is incorrect');
+			}
+		}
+		$this->set(array(
+			'title_for_layout' => 'Log in'
+		));
+	}
 }
