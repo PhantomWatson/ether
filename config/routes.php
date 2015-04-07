8501b016e354f2b70452410f8dd78ffc8e09ab5b
Graham Watson <graham@phantomwatson.com>
2015-04-06 22:38:19 -0400

    Remove comments

--- config/routes.php
+++ config/routes.php
@@ -42,16 +42,7 @@ use Cake\Routing\Router;
 Router::defaultRouteClass('Route');
 
 Router::scope('/', function ($routes) {
-    /**
-     * Here, we are connecting '/' (base path) to a controller called 'Pages',
-     * its action called 'display', and we pass a param to select the view file
-     * to use (in this case, src/Template/Pages/home.ctp)...
-     */
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'home']);
-
-    /**
-     * ...and connect the rest of 'Pages' controller's URLs.
-     */
     $routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
     /**
