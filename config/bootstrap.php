c939600b763ddf72f9e15c164bb84e12bfe4f351
Graham Watson <graham@phantomwatson.com>
2015-04-06 21:08:39 -0400

    Add Recaptcha plugin as submodule

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -180,6 +180,7 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
+Plugin::load('Recaptcha', ['routes' => true, 'bootstrap' => true]);
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
