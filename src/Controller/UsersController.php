de012c61c15699d75380a3b78817b2aef567dacb
Graham Watson <graham@phantomwatson.com>
2015-04-06 21:35:40 -0400

    Change add() into register() and fill with old site's code

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -36,24 +36,56 @@ class UsersController extends AppController
         $this->set('user', $user);
     }
 
-    /**
-     * Add method
-     *
-     * @return void
-     */
-    public function add()
+    public function register()
     {
+    	$this->loadComponent('Recaptcha.Recaptcha');
         $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->data);
-            if ($this->Users->save($user)) {
-                $this->Flash->success('The user has been saved.');
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The user could not be saved. Please, try again.');
-            }
-        }
-        $this->set(compact('user'));
+        	if ($this->Recaptcha->verify()) {
+	        	$user = $this->Users->patchEntity($user, $this->request->data);
+
+	        	// Clean email and color
+	        	$clean_email = trim($user->email);
+				$clean_email = strtolower($clean_email);
+				$clean_color = strtolower($user->color);
+				$clean_color = preg_replace("/[^a-z0-9]/", '', $clean_color);
+
+				$user->set([
+					'password' => $this->request->data['User']['password'],
+					'email' => $clean_email,
+					'color' => $clean_color,
+					'password_version' => 3
+				]);
+
+	            if ($this->Users->save($user)) {
+	                $this->Flash->success('Your account has been registered. You may now log in.');
+	                return $this->redirect(['action' => 'login']);
+	            } else {
+	                $this->Flash->error('There was an error registering your account. Please try again.');
+	            }
+			} else {
+				$this->Flash->error('Invalid CAPTCHA response');
+			}
+        } else {
+        	// Select a random available color
+			do {
+				$this->request->data['color'] = '#';
+				for ($n = 1; $n <= 3; $n++) {
+					$this->request->data['color'] .= str_pad(dechex(rand(0, 250)), 2, '0', STR_PAD_LEFT);
+				}
+				$isTaken = $this->User->colorIsTaken($this->request->data['color']);
+			} while ($isTaken);
+			$this->set('random_color', true);
+		}
+
+		/* So the password fields aren't filled out automatically when the user
+		 * is bounced back to the page by a validation error */
+		$this->request->data['User']['new_password'] = null;
+	    $this->request->data['User']['confirm_password'] = null;
+
+        $this->set([
+        	'title_for_layout' => 'Register Account',
+		]);
     }
 
     /**
