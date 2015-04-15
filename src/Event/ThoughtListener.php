29c245b82940e09b25b7a29e7d072171a2d803b6
Graham Watson <graham@phantomwatson.com>
2015-04-15 13:04:07 -0400

    Add and register parseThought()

--- src/Event/ThoughtListener.php
+++ src/Event/ThoughtListener.php
@@ -2,6 +2,7 @@
 namespace App\Event;
 
 use Cake\Event\EventListenerInterface;
+use Cake\ORM\TableRegistry;
 
 class ThoughtListener implements EventListenerInterface
 {
@@ -9,7 +10,10 @@ class ThoughtListener implements EventListenerInterface
 	public function implementedEvents()
 	{
 		return [
-			'Model.Thought.created' => 'updatePopulatedThoughtwords',
+			'Model.Thought.created' => [
+				'updatePopulatedThoughtwords',
+				'parseThought'
+			]
 		];
 	}
 
@@ -20,5 +24,18 @@ class ThoughtListener implements EventListenerInterface
 		// Check to see if the count of thoughts with this word is 1
 		// If so, it's a new addition to the list of populated thoughtwords
 		// and the cached list should be cleared and rewritten here (so the slight delay is experienced by the poster, not the next viewer)
+	}
+
+	public function parseThought($event, $entity, $options)
+	{
+		// Don't bother re-parsing thought if it hasn't changed
+		if (! $entity->dirty('thought')) {
+			return;
+		}
+
+		$thoughts = TableRegistry::get('Thoughts');
+		$parsedThought = $tables->linkThoughtwords($event->data['thought']);
+		$entity->set('parsedTextCache', $parsedThought);
+		$thoughts->save($entity);
 	}
 }
