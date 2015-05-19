b14fb5f3348df7b3aa77b12a4ab35269e10272b4
Graham Watson <graham@phantomwatson.com>
2015-05-19 19:21:46 -0400

    Reduce fields fetched for Thought

--- src/Model/Table/ThoughtsTable.php
+++ src/Model/Table/ThoughtsTable.php
@@ -271,6 +271,7 @@ class ThoughtsTable extends Table
     public function getFromWord($word)
     {
         return $this->find('all')
+            ->select(['id', 'user_id', 'word', 'thought', 'comments_enabled', 'parsedTextCache', 'anonymous', 'created', 'modified'])
             ->where(['word' => $word])
             ->order(['Thoughts.created' => 'DESC'])
             ->contain([
