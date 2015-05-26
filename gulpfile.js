f86f41db6b8cee3c1b0dc67e467125938124406e
Graham Watson <graham@phantomwatson.com>
2015-05-26 16:27:56 -0400

    Create gulp task for compiling style.less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
 
+var less = require('gulp-less');
+var path = require('path');
+gulp.task('less', function () {
+  return gulp.src('./webroot/css/style.less')
+    .pipe(less({
+      paths: [ path.join(__dirname) ]
+    }))
+    .pipe(gulp.dest('./webroot/css'));
+});
+
 gulp.task('default', function() {
 
 });
