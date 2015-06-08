e14f196f27b4f14fa106ac6f9cb7dfc56252f149
Graham Watson <graham@phantomwatson.com>
2015-06-08 16:23:11 -0400

    Add automatic CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,22 @@ var less = require('gulp-less');
 var watchLess = require('gulp-watch-less');
 var plumber = require('gulp-plumber');
 var notify = require("gulp-notify");
+var LessPluginCleanCSS = require('less-plugin-clean-css');
 
 gulp.task('default', ['less']);
 
 gulp.task('less', function () {
+	var cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
+	var lessConfig = {
+		plugins: [cleanCSSPlugin]
+	};
 	gulp.src('webroot/css/style.less')
-		.pipe(less())
+		.pipe(less(lessConfig))
         .pipe(gulp.dest('webroot/css'))
         .pipe(notify('LESS compiled'));
 	
 	watchLess('webroot/css/style.less')
-		.pipe(less())
+		.pipe(less(lessConfig))
         .pipe(gulp.dest('webroot/css'))
         .pipe(notify('LESS compiled'));
 });
