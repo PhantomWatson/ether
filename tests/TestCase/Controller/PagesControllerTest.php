46f97d621c8c031de32a4ec72a4d2b7a80a668d0
Graham Watson <graham@phantomwatson.com>
2015-06-11 16:03:09 -0400

    Initial commit

---
+++ tests/TestCase/Controller/PagesControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\PagesController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\PagesController Test Case
+ */
+class PagesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'Thoughts' => 'app.thoughts',
+        'Users' => 'app.users',
+        'Comments' => 'app.comments'
+    ];
+
+    /**
+     * Test home method
+     *
+     * @return void
+     */
+    public function testHome()
+    {
+        $this->get('/');
+        $this->assertResponseOk();
+    }
+
+    /**
+     * Test about method
+     *
+     * @return void
+     */
+    public function testAbout()
+    {
+        $this->get('/about');
+        $this->assertResponseOk();
+    }
+}
