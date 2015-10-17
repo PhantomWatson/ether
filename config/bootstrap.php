1c064bac0ef6a540b2d61e27f40783a96b2236fd
Graham Watson <graham@phantomwatson.com>
2015-10-17 16:50:43 -0400

    Replace AutoLogin plugin with CookieAuth

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,8 +181,8 @@ Request::addDetector('tablet', function ($request) {
 
 Plugin::load('Migrations');
 Plugin::load('Recaptcha', ['routes' => true, 'bootstrap' => true]);
-Plugin::load('AutoLogin');
 Plugin::load('Gourmet/CommonMark');
+Plugin::load('Xety/Cake3CookieAuth');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
