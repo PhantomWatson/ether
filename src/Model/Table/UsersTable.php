6a9c0f92e7b724c655bbc38a080e4ef8e05076ce
Graham Watson <graham@phantomwatson.com>
2015-10-25 22:08:37 -0400

    Remove validation for unused 'username' field

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -83,7 +83,6 @@ class UsersTable extends Table
      */
     public function buildRules(RulesChecker $rules)
     {
-        $rules->add($rules->isUnique(['username']));
         $rules->add($rules->isUnique(['email']));
         return $rules;
     }
