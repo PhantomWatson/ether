063a50bcba5288959ed882b9ab9142ec455910fe
Graham Watson <graham@phantomwatson.com>
2015-11-25 20:49:22 -0500

    Remove automatically baked and unused methods

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -19,34 +19,6 @@ class CommentsController extends AppController
     }
 
     /**
-     * Index method
-     *
-     * @return void
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Thoughts', 'Users']
-        ];
-        $this->set('comments', $this->paginate($this->Comments));
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Comment id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function view($id = null)
-    {
-        $comment = $this->Comments->get($id, [
-            'contain' => ['Thoughts', 'Users']
-        ]);
-        $this->set('comment', $comment);
-    }
-
-    /**
      * Add method
      *
      * @return void
@@ -66,51 +38,6 @@ class CommentsController extends AppController
                 return  $this->redirect($this->request->referer());
             }
         }
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Comment id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function edit($id = null)
-    {
-        $comment = $this->Comments->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $comment = $this->Comments->patchEntity($comment, $this->request->data);
-            if ($this->Comments->save($comment)) {
-                $this->Flash->success('The comment has been saved.');
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The comment could not be saved. Please, try again.');
-            }
-        }
-        $thoughts = $this->Comments->Thoughts->find('list', ['limit' => 200]);
-        $users = $this->Comments->Users->find('list', ['limit' => 200]);
-        $this->set(compact('comment', 'thoughts', 'users'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Comment id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $comment = $this->Comments->get($id);
-        if ($this->Comments->delete($comment)) {
-            $this->Flash->success('The comment has been deleted.');
-        } else {
-            $this->Flash->error('The comment could not be deleted. Please, try again.');
-        }
-        return $this->redirect(['action' => 'index']);
     }
 
     public function refreshFormatting($commentId)
