5e55b14587763e970c623801eafdff6b35ee6e76
Graham Watson <graham@phantomwatson.com>
2015-12-09 20:41:57 -0500

    Remove auto-baked actions

--- src/Controller/MessagesController.php
+++ src/Controller/MessagesController.php
@@ -97,51 +97,6 @@ class MessagesController extends AppController
         }
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Message id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function edit($id = null)
-    {
-        $message = $this->Messages->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $message = $this->Messages->patchEntity($message, $this->request->data);
-            if ($this->Messages->save($message)) {
-                $this->Flash->success('The message has been saved.');
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The message could not be saved. Please, try again.');
-            }
-        }
-        $recipients = $this->Messages->Recipients->find('list', ['limit' => 200]);
-        $senders = $this->Messages->Senders->find('list', ['limit' => 200]);
-        $this->set(compact('message', 'recipients', 'senders'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Message id
-     * @return void
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $message = $this->Messages->get($id);
-        if ($this->Messages->delete($message)) {
-            $this->Flash->success('The message has been deleted.');
-        } else {
-            $this->Flash->error('The message could not be deleted. Please, try again.');
-        }
-        return $this->redirect(['action' => 'index']);
-    }
-
     public function conversation($penpalColor = null)
     {
         $userId = $this->Auth->user('id');
