1ab983a46140f2b8aa5cca746fc9e373c975bbcc
Graham Watson <graham@phantomwatson.com>
2018-01-01 19:15:28 -0500

    Add /colors/get-name

--- src/Controller/ColorsController.php
+++ src/Controller/ColorsController.php
@@ -1,15 +1,27 @@
 <?php
 namespace App\Controller;
 
+use App\Color\Color;
 use App\Model\Table\UsersTable;
+use Cake\Network\Exception\NotFoundException;
 use Cake\ORM\TableRegistry;
 
 class ColorsController extends AppController
 {
+    /** @var Color */
+    private $Color;
+
+    /**
+     * Initialize method
+     *
+     * @return void
+     */
     public function initialize()
     {
         parent::initialize();
+        $this->loadComponent('RequestHandler');
         $this->Auth->allow();
+        $this->Color = new Color();
     }
 
     /**
@@ -22,7 +34,6 @@ class ColorsController extends AppController
         /** @var UsersTable $usersTable */
         $usersTable = TableRegistry::get('Users');
         $colors = $usersTable->getColorsWithThoughts();
-        $Color = new \App\Color\Color();
         $hexCodes = [];
         foreach ($colors as $section => $sectionColors) {
             foreach ($sectionColors as $color => $count) {
@@ -31,8 +42,32 @@ class ColorsController extends AppController
         }
 
         $this->set([
-            'colors' => $Color->getClosestXkcdColors($hexCodes),
+            'colors' => $this->Color->getClosestXkcdColors($hexCodes),
             'title_for_layout' => 'Color Names'
+        ]);
+    }
+
+    /**
+     * Renders /colors/get-name
+     *
+     * @param string|null $color Six-character hex code for a color
+     * @return void
+     * @throws NotFoundException
+     */
+    public function getName($color = null)
+    {
+        $this->viewBuilder()->setClassName('Json');
+
+        $name = $this->Color->getClosestXkcdColor($color);
+
+        if (! $name) {
+            $msg = 'Sorry, Ether can\'t figure out the name of the color with the hex code "' . $color . '"';
+            throw new NotFoundException($msg);
+        }
+
+        $this->set([
+            'name' => $name,
+            '_serialize' => ['name']
         ]);
     }
 }
