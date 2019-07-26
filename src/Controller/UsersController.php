430210c1238e3a9c2baaa6fc0c8a4bb107c96df3
Graham Watson <graham@phantomwatson.com>
2019-07-26 11:38:06 -0400

    Remove unused edit and delete methods

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -154,53 +154,6 @@ class UsersController extends AppController
     }
 
     /**
-     * Edit method
-     *
-     * @param string|null $id User id
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function edit($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success('The user has been saved.');
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error('The user could not be saved. Please, try again.');
-            }
-        }
-        $this->set(compact('user'));
-
-        return null;
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id User id
-     * @return \Cake\Http\Response
-     * @throws \Cake\Network\Exception\NotFoundException
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success('The user has been deleted.');
-        } else {
-            $this->Flash->error('The user could not be deleted. Please, try again.');
-        }
-
-        return $this->redirect(['action' => 'index']);
-    }
-
-    /**
      * Renders /users/login
      *
      * @return \Cake\Http\Response|null
