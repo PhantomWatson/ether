5985789cf0c709b44edb9274c5194bca84c4e2dd
Graham Watson <graham@phantomwatson.com>
2021-11-21 22:23:07 -0500

    Refactor like crazy

--- webroot/js/ether-markov.js
+++ webroot/js/ether-markov.js
@@ -1,17 +1,23 @@
 /**
- * A Markov-chain text generator developed for theEther.com 
+ * A Markov-chain text generator developed for theEther.com
  */
-var EtherMarkov = {
+const EtherMarkov = {
     blockCount: 0,
     blockLength: null,
+    buttonStart: document.getElementById('start'),
+    buttonStop: document.getElementById('stop'),
     currentBlock: null,
     entropyScore: 0,
+    entropyScoreContainer: document.getElementById('entropyScore'),
     interval: null,
     limit: null,
     loop: null,
-    processingContainer: null,
-    resultsContainer: null,
+    processingContainer: document.getElementById('markovOptions'),
+    resultsContainer: document.getElementById('markovResults'),
     seed: null,
+    selectorBlockLength: document.getElementById('blockLength'),
+    selectorLimit: document.getElementById('limit'),
+    selectorSpeed: document.getElementById('speed'),
     sourceUrl: null,
 
     /**
@@ -22,91 +28,83 @@ var EtherMarkov = {
      * @param {string} sourceUrl
      */
     init: function (sourceUrl) {
-        var blockLength = $('#blockLength');
-        var speed = $('#speed');
-        var limit = $('#limit');
-
-        this.blockLength = blockLength.val();
-        this.interval = 1000 / speed.val();
-        this.limit = limit.val();
-        this.resultsContainer = $('#markovResults');
-        this.processingContainer = $('#markovOptions');
-        this.entropyScoreContainer = $('#entropyScore');
+        const selectVal = (select) => select.querySelector('option:checked').value;
+        this.blockLength = selectVal(this.selectorBlockLength);
+        this.interval = 1000 / selectVal(this.selectorSpeed);
+        this.limit = selectVal(this.selectorLimit);
         this.sourceUrl = sourceUrl;
-        
-        $('#start').click(function () {
-            EtherMarkov.start();
-        });
-        $('#stop').click(function () {
-            EtherMarkov.stop();
-        }).hide();
-        blockLength.change(function () {
-            EtherMarkov.blockLength = $(this).val();
-        });
-        speed.change(function () {
-            EtherMarkov.interval = 1000 / $(this).val();
-        });
-        limit.change(function () {
-            EtherMarkov.limit = $(this).val();
-        });
-        
+        this.buttonStart.addEventListener('click', () => EtherMarkov.start());
+        this.buttonStop.addEventListener('click', () => EtherMarkov.stop());
+        this.buttonStop.style.display = 'none';
+        this.selectorBlockLength.addEventListener(
+            'change',
+            event => this.blockLength = selectVal(event.target)
+        );
+        this.selectorSpeed.addEventListener(
+            'change',
+            event => this.interval = 1000 / selectVal(event.target)
+        );
+        this.selectorLimit.addEventListener(
+            'change',
+            event => this.limit = selectVal(event.target)
+        );
+
         this.setup();
     },
-    
+
     setup: function () {
-        var start = $('#start');
-        start.prop('disabled', true).hide();
-        
-        start.after('<p id="loadingSource">Loading source text... <img src="/img/loading_small.gif" alt="Loading..." /></p>');
-        this.getSeed(function () {
-            start.prop('disabled', false).show();
-            $('#loadingSource').hide();
+        this.buttonStart.disabled = true;
+        this.buttonStart.style.display = 'none';
+
+        const loading = document.createElement('p');
+        loading.id = 'loadingSource';
+        loading.innerHTML = 'Loading source text... <img src="/img/loading_small.gif" alt="Loading..." />';
+
+        this.buttonStart.after(loading);
+        this.getSeed(() => {
+            this.buttonStart.disabled = false;
+            this.buttonStart.style.display = 'inline';
+            loading.remove();
         });
     },
-    
+
     getSeed: function (successCallback) {
-        var displayError = function () {
-            alert('AHFUCK SOMETHING WENT WRONG');
-        };
+        const displayError = () => alert('AHFUCK SOMETHING WENT WRONG');
         $.ajax({
             url: this.sourceUrl,
             dataType: 'json',
-            beforeSend: function () {
-            },
-            success: function (data) {
+            beforeSend: () => {},
+            success: (data) => {
                 if (data.hasOwnProperty('source') && data.source) {
-                    EtherMarkov.seed = data.source;
+                    this.seed = data.source;
                     successCallback();
                 } else {
                     displayError();
                 }
             },
-            error: function () {
-                displayError();
-            }
+            error: () => displayError()
         });
     },
-    
+
     start: function () {
-        $('#generator-interface').find('hr').show();
-        $('#randomness-label').show();
-        this.entropyScoreContainer.css({display: 'inline-block'});
-        $('#start').hide();
-        $('#stop').show();
-        $('#blockLength').prop('disabled', true);
-        $('#speed').prop('disabled', true);
-        $('#limit').prop('disabled', true);
+        document.getElementById('randomness-label').style.display = 'inline';
+        this.entropyScoreContainer.style.display = 'inline-block';
+        this.buttonStart.style.display = 'none';
+        this.buttonStop.style.display = 'inline';
+        this.selectorBlockLength.disabled = true;
+        this.selectorBlockLength.title = 'Refresh the page to change randomness';
+        this.selectorSpeed.disabled = true;
+        this.selectorLimit.disabled = true;
         if (this.currentBlock === null) {
-            var start = this.getRandomBlock(this.seed, this.blockLength);
-            this.currentBlock = start.split(' ');
+            this.currentBlock = this.getRandomBlock(this.seed).split(' ');
             this.displayInResults(this.currentBlock.join(' '));
             this.blockCount += this.blockLength;
         }
-        this.loop = setInterval(function () {EtherMarkov.addBlock();}, this.interval);
+        this.loop = setInterval(() => this.addWord(), this.interval);
     },
-    
-    addBlock: function () {
-        var word = this.getNextWord();
+
+    addWord: function () {
+        const word = this.getNextWord();
         this.currentBlock.shift();
         this.currentBlock.push(word);
         this.displayInResults(word);
@@ -119,93 +117,124 @@ var EtherMarkov = {
         }
         this.updateDisplayedEntropyScore();
     },
-    
-    displayInResults: function (message) {
-        var text = $('<span> ' + message + '</span>');
-        text.css('opacity', 0);
-        this.resultsContainer.append(text);
-        var duration = 500 + (Math.round(Math.random()) ? -1 : 1) * (Math.random() * 400);
-        text.animate({opacity: 1}, duration);
+
+    displayInResults: function (word) {
+        const duration = 1500 + (Math.round(Math.random()) ? -1 : 1) * (Math.random() * 400);
+        let text = document.createElement('span');
+        text.innerHTML = ' ' + word;
+        this.resultsContainer.appendChild(text);
+
+        /* Delaying by 10ms prevents a problem where opacity will occasionally be set before the text appears,
+         * resulting in the CSS transition not being applied */
+        setTimeout(() => text.style.opacity = '1', 10);
     },
-    
+
     stop: function () {
-        $('#stop').hide();
-        $('#start').show();
-        $('#speed').prop('disabled', false);
-        $('#limit').prop('disabled', false);
+        this.buttonStop.style.display = 'none';
+        this.buttonStart.style.display = 'inline';
+        this.selectorSpeed.disabled = false;
+        this.selectorLimit.disabled = false;
         clearInterval(this.loop);
     },
-    
+
     getNextWord: function () {
-        var seed = this.seed;
-        var prevBlock = this.currentBlock.join(' ') + ' ';
-        var blockLength = this.blockLength;
-        var wordCandidates = [];
-        var searchStart = 0;
-        this.processingContainer.html(prevBlock+'<ul></ul>');
-        var wordList = this.processingContainer.find('ul');
-        var safeWord;
-        var wordListItem;
+        const prevBlock = this.currentBlock.join(' ') + ' ';
+        this.processingContainer.innerHTML = prevBlock + '<ul></ul>';
+        const wordList = this.processingContainer.querySelector('ul');
+
+        // Build the array of words that are candidates for appending to the chain
+        const wordCandidates = [];
+        let searchStart = 0;
         while (true) {
-            var matchPos = seed.indexOf(prevBlock, searchStart);
+            const matchPos = this.seed.indexOf(prevBlock, searchStart);
             if (matchPos === -1) {
                 break;
             }
-            var wordStart = matchPos + prevBlock.length;
-            var wordEnd = seed.indexOf(' ', wordStart + 1);
-            var wordLength = wordEnd - wordStart;
-            var word = seed.substr(wordStart, wordLength);
+
+            const wordStart = matchPos + prevBlock.length;
+            const wordEnd = this.seed.indexOf(' ', wordStart + 1);
+            const wordLength = wordEnd - wordStart;
+            const word = this.seed.substr(wordStart, wordLength);
             wordCandidates.push(word);
-            
-            safeWord = encodeURI(word);
-            var displayedWord = this.processingContainer.find('li[data-word="'+safeWord+'"]');
-            if (displayedWord.length === 0) {
-                wordListItem = $('<li>' + word + '</li>').data('word', safeWord);
-                wordList.append(wordListItem);
-            } else {
-                var countElement = displayedWord.find('span.count');
-                if (countElement.length === 0) {
-                    displayedWord.append(' <span class="count">2</span>');
-                } else {
-                    var count = parseInt(countElement.html());
-                    countElement.html(count + 1);
-                }
-            }
-            
+
             searchStart = wordEnd + 1;
         }
-        if (wordCandidates.length > 0) {
-            if (wordCandidates.length > 1) {
-                this.entropyScore++;
-            }
-            var key = Math.floor(Math.random() * wordCandidates.length);
-            var chosenWord = wordCandidates[key];
-            var chosenListItem = wordList.find('li').filter(function () {
-                return $(this).text() === chosenWord;
-            });
-            chosenListItem.addClass('selected');
-            return chosenWord;
+
+        if (wordCandidates.length === 0) {
+            return this.getRandomBlock();
         }
-        return this.getRandomBlock(seed, blockLength);
+
+        // Add all candidates to the "thinkin" container
+        wordCandidates.forEach(word => this.addWordToProcessingContainer(wordList, word));
+
+        // Advance entropy score if there's more than one choice
+        if (wordCandidates.length > 1) {
+            this.entropyScore++;
+        }
+
+        // Mark the chosen word as selected and return it
+        const chosenWord = this.getRandomArrayElement(wordCandidates);
+        const words = Array.from(wordList.querySelectorAll('li'));
+        for (let i = 0; i < words.length; i++) {
+            if (words[i].innerText === chosenWord) {
+                words[i].classList.add('selected');
+            }
+        }
+        return chosenWord;
     },
-    
-    getRandomBlock: function (seed, blockLength) {
+
+    getRandomBlock: function () {
         while (true) {
-            var startPos = Math.floor(Math.random() * seed.length);
-            var blockStart = seed.indexOf(' ', startPos) + 1;
-            var blockEnd = blockStart;
-            for (var i = 1; i <= blockLength; i++) {
-                blockEnd = seed.indexOf(' ', blockEnd + 1);
+            const startPos = Math.floor(Math.random() * this.seed.length);
+            const blockStart = this.seed.indexOf(' ', startPos) + 1;
+            let blockEnd = blockStart;
+            for (let i = 1; i <= this.blockLength; i++) {
+                blockEnd = this.seed.indexOf(' ', blockEnd + 1);
             }
             if (blockStart > -1 && blockEnd > -1) {
-                return seed.substr(blockStart, (blockEnd - blockStart));
+                return this.seed.substr(blockStart, blockEnd - blockStart);
             }
         }
     },
-    
+
     updateDisplayedEntropyScore: function () {
-        var percent = Math.round((this.entropyScore / this.blockCount) * 100);
-        this.entropyScoreContainer.find('span').html(percent + '%');
-        this.entropyScoreContainer.find('.progress-bar').css({width: percent + '%'})
+        const percent = Math.round((this.entropyScore / this.blockCount) * 100);
+        this.entropyScoreContainer.querySelector('span').innerText = percent + '%';
+        this.entropyScoreContainer.querySelector('.progress-bar').style.width = percent + '%';
+    },
+
+    /**
+     * Either display the candidate word or increment its ×2, ×3, etc. counter
+     *
+     * @param {HTMLUListElement} wordList
+     * @param {string} word
+     */
+    addWordToProcessingContainer(wordList, word) {
+        let safeWord = encodeURI(word);
+        const displayedWord = this.processingContainer.querySelector(`li[data-word="${safeWord}"]`);
+        if (displayedWord) {
+            const countElement = displayedWord.querySelectorAll('span.count');
+            if (countElement.length) {
+                countElement.innerHtml = parseInt(countElement.innerHtml) + 1;
+            } else {
+                displayedWord.innerHTML += ' <span class="count">2</span>';
+            }
+        } else {
+            let wordListItem = document.createElement('li');
+            wordListItem.innerText = word;
+            wordListItem.dataset.word = safeWord;
+            wordList.appendChild(wordListItem);
+        }
+    },
+
+    /**
+     * Returns a random element from the provided array
+     *
+     * @param {Array} array
+     * @returns {*}
+     */
+    getRandomArrayElement(array) {
+        const key = Math.floor(Math.random() * array.length);
+        return array[key];
     }
 };
