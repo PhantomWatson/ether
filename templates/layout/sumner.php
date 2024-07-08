9dfafefc72b5fff90ea3146337ec4779f31e0c58
Graham Watson <graham@phantomwatson.com>
2024-07-08 17:52:35 -0400

    Change SumnerHub title

--- templates/layout/sumner.php
+++ templates/layout/sumner.php
@@ -83,9 +83,7 @@
 <html>
 
 <head>
-    <title>colour_blue</title>
-    <meta name="description" content="website description" />
-    <meta name="keywords" content="website keywords, website keywords" />
+    <title>SumnerHub</title>
     <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
     <meta http-equiv="robots" content="noindex, nofollow" />
     <link rel="stylesheet" type="text/css" href="/css/sumner.css" title="style" />
