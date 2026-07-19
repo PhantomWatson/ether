83b09e62b0fff14cac1d11a3ad4119e37d49ddd6
Graham Watson <graham@phantomwatson.com>
2026-07-19 04:44:01 +0000

    Update error pages via skeleton app
    plus using default layout

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -1,44 +1,26 @@
 <?php
 /**
  * @var \App\View\AppView $this
- * @var object $error
- * @var mixed $message
- * @var mixed $url
+ * @var string $message
+ * @var string $url
  */
 use Cake\Core\Configure;
 
-if (Configure::read('debug')):
-    $this->layout = 'dev_error';
+$this->setLayout('default');
+
+if (Configure::read('debug')) :
+    $this->setLayout('dev_error');
 
     $this->assign('title', $message);
-    $this->assign('templateName', 'error400.ctp');
+    $this->assign('templateName', 'error400.php');
 
     $this->start('file');
-?>
-<?php if (!empty($error->queryString)) : ?>
-    <p class="notice">
-        <strong>SQL Query: </strong>
-        <?= h($error->queryString) ?>
-    </p>
-<?php endif; ?>
-<?php if (!empty($error->params)) : ?>
-        <strong>SQL Query Params: </strong>
-        <?= Debugger::dump($error->params) ?>
-<?php endif; ?>
-<?= $this->element('auto_table_warning') ?>
-<?php
-    if (extension_loaded('xdebug')):
-        xdebug_print_function_stack();
-    endif;
-
+    echo $this->element('auto_table_warning');
     $this->end();
 endif;
 ?>
 <h2><?= h($message) ?></h2>
 <p class="error">
     <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= sprintf(
-        __d('cake', 'The requested address %s was not found on this server.'),
-        "<strong>'{$url}'</strong>"
-    ) ?>
+    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
 </p>
